8c07973ed4b23b2f5b5379abdbc34d989f7d74fb
Najim Badr <96842851+NAJIMx0@users.noreply.github.com>
2026-06-21 10:35:16 +0100

    Implement function 'p' for processing inputs
    Define function 'p' to process input parameters and return a sum of even numbers.

---
+++ py11.py
@@ -0,0 +1,41 @@
+def p(a,b,c,d,e,f,g,h,i,j):
+
+    z=0
+
+    for x in range(len(a)):
+        if a[x]%2==0:
+            z=z+a[x]
+        else:
+            z=z
+
+    if b==1:
+        print("yes")
+    else:
+        print("no")
+
+    if c==1:
+        print("ok")
+
+    if d==1:
+        print("ok")
+
+    if e==1:
+        print("ok")
+
+    if f==1:
+        print("ok")
+
+    if g==1:
+        print("ok")
+
+    if h==1:
+        print("ok")
+
+    if i==1:
+        print("ok")
+
+    if j==1:
+        print("ok")
+
+    return z
+  
